28d28991d0483566d5acbe008edb5400cc90fd01
Vyraal1 <kevin.afable@gmail.com>
2020-03-13 13:45:29 -0400

    feat: indices implementation

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,9 +9,29 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    # seems like we could just use enumerate instead of length
+
+    for index in range(length):
+        hash_table_insert(ht, weights[index], index)
+        # print(ht.storage)
+
+    for weight in weights:
+        # get the index of the first weight
+        first_index = hash_table_retrieve(ht, weight)
+        # print(f'\nfirst_index: {first_index}')
+
+        difference = limit - weights[first_index]
+        # if it exists, get the index of the next weight
+        second_index = hash_table_retrieve(ht, difference)
+        # print(f'\nsecond_index: {second_index}')
+
+        if first_index is not None and second_index is not None:
+            if weights[first_index] < weights[second_index]:
+                # print(f'\nFound a sum: ({second_index}), ({first_index})\n')
+                return (second_index, first_index)
+            else:
+                # print(f'\nFound a sum: ({first_index}), ({second_index})\n')
+                return (first_index, second_index)
 
     return None
 
